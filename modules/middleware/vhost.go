1cf7c86f578e475b172f39534893ccc0b1b6d59d
Adrian Pistol <adinator1999@gmail.com>
2015-06-18 02:15:32 +0200

    VHOSTs

---
+++ modules/middleware/vhost.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"regexp"
+)
+
+// Dynamic routing based on host given by a map.
+func VHOST(plan Plan) func(*gin.Context) {
+	portmatch := regexp.MustCompile(":.*$")
+	return func(c *gin.Context) {
+		host := c.Request.Host
+		if plan[host] != nil {
+			plan[host](c)
+			return
+		}
+		hostwithoutport := portmatch.ReplaceAllLiteralString(host, "")
+		if plan[hostwithoutport] != nil {
+			plan[host](c)
+			return
+		}
+		if plan["***"] != nil {
+			plan["***"](c)
+			return
+		}
+		c.Next()
+	}
+}
