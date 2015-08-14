92e58f3dfbee4b777b33b1e32eeed8cacd02feb5
Adrian Pistol <adinator1999@gmail.com>
2015-08-14 18:45:23 +0200

    Gah.

--- modules/helpers/html.go
+++ modules/helpers/html.go
@@ -4,6 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func String(context *gin.Context, responsecode int, text string) {
+	context.Data(responsecode, "text/plain", []byte(text))
+}
+
 func HTMLString(context *gin.Context, responsecode int, html string) {
 	context.Data(responsecode, "text/html", []byte(html))
 }
