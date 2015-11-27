a374362442cacfb64a8a10b9c506a0249b1b646f
Adrian Pistol <adinator1999@gmail.com>
2015-11-27 12:59:29 +0100

    Fixing weird bugs, hooray.

--- modules/static/static.go
+++ modules/static/static.go
@@ -67,7 +67,7 @@ func (l *localFileSystem) Open(name string) (http.File, error) {
 	//if !l.physfs {
 	newname := name
 	if p := strings.TrimPrefix(name, l.prefix); len(p) <= len(name) {
-		newname = path.Join(l.root, name)
+		newname = path.Join(l.root, p)
 	}
 	fmt.Println("Open: "+newname)
 	f, err := l.Open(newname)
