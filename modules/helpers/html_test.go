ed2b712d30722bc872b71432a1fdbb6d63e834c8
Adrian Pistol <adinator1999@gmail.com>
2015-11-29 22:09:37 +0100

    My first test. I can't test it yet, but judging from my commit history, it doesn't look too good.

---
+++ modules/helpers/html_test.go
@@ -0,0 +1,20 @@
+package helpers
+
+import (
+	"github.com/carbonsrv/carbon/ctest"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHTMLHelpers(t *testing.T) {
+	r := gin.New()
+	router.GET("/string", func(c *gin.Context) {
+		String(c, 200, "Hello world!")
+	})
+
+	w := ctest.Request(r, "GET", "/string")
+	assert.Equal(t, w.Code, 200)
+	assert.Equal(t, string(w.Body) == "Hello world!")
+	assert.Equal(t, w.HeaderMap.Get("Content-Type"), "text/plain")
+}
