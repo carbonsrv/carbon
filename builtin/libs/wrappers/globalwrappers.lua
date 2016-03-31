6e706b4d37bb7b12eb661b84b46d4abc4574b8a4
Adrian Pistol <adinator1999@gmail.com>
2016-03-31 15:01:56 +0200

    Add string modulo overload.

--- builtin/libs/wrappers/globalwrappers.lua
+++ builtin/libs/wrappers/globalwrappers.lua
@@ -480,3 +480,14 @@ function syntaxhl(text, customcss)
 		return carbon._syntaxhl(text, "")
 	end
 end
+
+-- overload modulo for strings to allow formatting
+getmetatable("").__mod = function(a, b)
+	if not b then
+		return a
+	elseif type(b) == "table" then
+		return string.format(a, unpack(b))
+	else
+		return string.format(a, b)
+	end
+end
